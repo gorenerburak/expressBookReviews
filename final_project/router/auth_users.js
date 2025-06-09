5555d5d05f7e9f2c933b2968530aee13bb2e6b87
gorenerburak <gorenerburak@gmail.com>
2025-06-09 06:54:08 -0400

    /customer/auth/*

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const jwt = require('jsonwebtoken');
 let books = require("./booksdb.js");
-const regd_users = express.Router();
+const registered_users = express.Router();
 
 let users = [];
 
@@ -14,17 +14,17 @@ const authenticatedUser = (username,password)=>{ //returns boolean
 }
 
 //only registered users can login
-regd_users.post("/login", (req,res) => {
+registered_users.post("/login", (req,res) => {
   //Write your code here
   return res.status(300).json({message: "Yet to be implemented"});
 });
 
 // Add a book review
-regd_users.put("/auth/review/:isbn", (req, res) => {
+registered_users.put("/auth/review/:isbn", (req, res) => {
   //Write your code here
   return res.status(300).json({message: "Yet to be implemented"});
 });
 
-module.exports.authenticated = regd_users;
+module.exports.authenticated = registered_users;
 module.exports.isValid = isValid;
 module.exports.users = users;
